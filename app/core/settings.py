83bf35b38dae461eb317d659c7000d9eb0c0208f
Basil Sunny <csbasil@gmail.com>
2022-09-14 13:14:45 +0530

    feat: implement data driven unit test suit

--- app/core/settings.py
+++ app/core/settings.py
@@ -4,4 +4,8 @@ Import all environment variables here and setup all project configurations
 
 from os import environ
 
+# database settings
 DATABASE_URL = environ.get("DATABASE_URL", None)
+
+# development settings
+DEBUG = bool(environ.get("DEBUG", 0))
